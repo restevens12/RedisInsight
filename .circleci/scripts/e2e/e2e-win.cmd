a21687a56556d1cce3c627b3236e1edb8cefb710
Artem <artem.horuzhenko@softeq.com>
2022-01-18 13:27:24 +0300

    set proper node version

--- .circleci/scripts/e2e/e2e-win.cmd
+++ .circleci/scripts/e2e/e2e-win.cmd
@@ -5,16 +5,17 @@ set ELECTRON_PATH=%USERPROFILE%/AppData/Local/Programs/redisinsight/RedisInsight
 
 call yarn --cwd tests/e2e install
 
-call "./release/Redisinsight-preview-win-installer.exe"
+call "./release/RedisInsight-preview-win-installer.exe"
 
 :: waiting until app auto launches
 timeout 5
 
 :: close an auto launched app
-taskkill /im Redisinsight-preview.exe /f
+taskkill /im RedisInsight-preview.exe /f
+taskkill /im RedisInsight-preview-win-installer.exe /f
 
 :: run rte & tests
-call docker-compose -f tests/e2e/docker-compose.yml create --force-recreate
-call docker-compose -f tests/e2e/docker-compose.yml run init-rte || exit 1
+:: call docker-compose -f tests/e2e/docker-compose.yml create --force-recreate
+:: call docker-compose -f tests/e2e/docker-compose.yml run init-rte || exit 1
 
 call yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop
