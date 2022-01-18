1eaacc3945d58ea19c7d01ce88ebdc4db0b94e82
Artem <artem.horuzhenko@softeq.com>
2022-01-18 13:44:10 +0300

    fix choco cmd destination

--- .circleci/scripts/e2e/e2e-win.cmd
+++ .circleci/scripts/e2e/e2e-win.cmd
@@ -19,3 +19,14 @@ taskkill /im RedisInsight-preview-win-installer.exe /f
 :: call docker-compose -f tests/e2e/docker-compose.yml run init-rte || exit 1
 
 call yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop
+
+"C:\Program Files\Docker\docker.exe"
+invoke-webRequest "https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -outfile "C:\Program Files\Docker\docker-compose.exe"
+https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-windows-x86_64.exe
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
+
+:: USE THIS!
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-windows-x86_64.exe" -UseBasicParsing -OutFile "C:\Program Files\Docker\docker-compose.exe"
+
+stop-service *docker*
+dockerd --experimental
