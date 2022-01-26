e474ab891da61fabbc2f06857725f08f73085e70
Artem <artem.horuzhenko@softeq.com>
2022-01-26 14:25:46 +0300

    macos debug

--- .circleci/scripts/e2e/e2e-mac.sh
+++ .circleci/scripts/e2e/e2e-mac.sh
@@ -6,14 +6,15 @@ sudo cp -R /Volumes/RedisInsight-*/RedisInsight-preview.app /Applications
 sudo hdiutil unmount /Volumes/RedisInsight-*/
 
 # run rte
-docker run -d -p 8100:6379 redislabs/redismod
-docker run -d -p 8101:6379 redis:5
-docker run -d -p 8102:6379 redislabs/redismod
-docker run -d -p 8101:6379 redis:5
+#docker run -d -p 8100:6379 redislabs/redismod
+#docker run -d -p 8101:6379 redis:5
+#docker run -d -p 8102:6379 redislabs/redismod
+#docker run -d -p 8101:6379 redis:5
+
 #docker-compose -f tests/e2e/docker-compose.yml create --force-recreate
 #docker-compose -f tests/e2e/docker-compose.yml run init-rte && \
 
 # run tests
-COMMON_URL=/Applications/RedisInsight-preview.app/Contents/resources/app.asar/index.html \
-ELECTRON_PATH=/Applications/RedisInsight-preview.app/Contents/redisinsight \
-yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop
+#sudo COMMON_URL=/Applications/RedisInsight-preview.app/Contents/Resources/app.asar/index.html \
+#ELECTRON_PATH=/Applications/RedisInsight-preview.app/Contents/MacOs/RedisInsight-preview \
+#yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop
