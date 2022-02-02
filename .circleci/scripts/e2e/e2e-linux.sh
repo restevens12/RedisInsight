00dbb2fff983926b0ef136fc1c3ed1065d1c69c5
Artem <artem.horuzhenko@softeq.com>
2022-02-02 13:40:18 +0300

    fix sentinel + cluster rtes

--- .circleci/scripts/e2e/e2e-linux.sh
+++ .circleci/scripts/e2e/e2e-linux.sh
@@ -4,6 +4,7 @@ yarn --cwd tests/e2e install
 ./release/*.AppImage --appimage-mount >> apppath &
 
 # run rte
+docker-compose -f tests/e2e/docker-compose.yml build
 docker-compose -f tests/e2e/docker-compose.yml create --force-recreate
 docker-compose -f tests/e2e/docker-compose.yml run init-rte && \
 
@@ -11,3 +12,5 @@ docker-compose -f tests/e2e/docker-compose.yml run init-rte && \
 COMMON_URL=$(tail -n 1 apppath)/resources/app.asar/index.html \
 ELECTRON_PATH=$(tail -n 1 apppath)/redisinsight \
 yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop
+
+docker-compose -f tests/e2e/docker-compose.yml stop
